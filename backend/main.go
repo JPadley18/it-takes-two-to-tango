48871980e2ee32b9d246c1d055c0e9fdf53b1efe
Jacob Padley <jacob5180@hotmail.co.uk>
2025-02-08 17:43:05 +0000

    Set development environment

--- backend/main.go
+++ backend/main.go
@@ -30,7 +30,7 @@ func main() {
 		},
 	}))
 
-	f.Get("/play/:lobby", websocket.New(handlers.HandlePlayerConnect))
+	f.Get("/play/:id", websocket.New(handlers.HandlePlayerConnect))
 	f.Get("/lobbies", handlers.HandleListLobbies)
 	f.Post("/start", handlers.HandleCreateLobby)
 
