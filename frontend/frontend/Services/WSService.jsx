8ca980d559ddbb2e1ab1f49932020551d4185ac9
Callum Shervill <shervc272@gmail.com>
2025-02-08 20:45:27 +0000

    more webservices

--- frontend/frontend/Services/WSService.jsx
+++ frontend/frontend/Services/WSService.jsx
@@ -1,9 +1,18 @@
-export default class WSService {
-  constructor(id) {
-    this.id = id;
-    this.ws = new WebSocket("ws://localhost:8080/play/" + id);
-    this.ws.onopen = () => {
-      console.log("Connected to server");
-    };
-  }
+import useWebSocket from "react-use-websocket";
+
+export default function WSService(props) {
+  const { sendJsonMessage, getWebSocket } = useWebSocket(
+    "ws://localhost:8080/play/" + props.id,
+    {
+      onOpen: () => console.log("Connected to server"),
+      onMessage: (event) => {
+        try {
+          const data = JSON.parse(event.data);
+          console.log(data);
+        } catch (e) {
+          console.error(e);
+        }
+      },
+    }
+  );
 }
--- frontend/frontend/src/Pages/Game/Game.jsx
+++ frontend/frontend/src/Pages/Game/Game.jsx
@@ -3,14 +3,42 @@ import "./Game.css";
 import Board from "../../Components/Board/Board";
 import gamedata from "./game.json";
 import gamedata2 from "./game2.json";
-import WSService from "../../../Services/WSService";
 import { useParams } from "react-router-dom";
+import useWebSocket from "react-use-websocket";
 
 export default function Game() {
   const { id } = useParams();
   console.log(id);
 
-  const ws = new WSService(id);
+  const { sendJsonMessage, getWebSocket } = useWebSocket(
+    "ws://localhost:8080/play/" + id,
+    {
+      onOpen: () => console.log("Connected to server"),
+      onMessage: (event) => {
+        try {
+          const message = event.data;
+          const data = JSON.parse(message);
+          console.log(data);
+          switch (data.type) {
+            case "update":
+              gamedata.data.board.symbols = data.board.symbols;
+              break;
+            case "win":
+              console.log("win");
+              break;
+            case "draw":
+              console.log("draw");
+              break;
+            default:
+              console.log("default");
+              break;
+          }
+        } catch (e) {
+          console.error(e);
+        }
+      },
+    }
+  );
 
   const printBothBoards = () => {
     console.log(gamedata.data.board.symbols);
