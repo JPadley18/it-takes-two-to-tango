364ff4ba89a0094094ef739945bbb83dbacb21d0
Joshua Martins-Borges <zlac269@live.rhul.ac.uk>
2025-02-08 15:24:41 +0000

    feat: added some functionality to db stuff

--- db/dbUtils.js
+++ db/dbUtils.js
@@ -21,6 +21,10 @@ async function closeDB(){
 userData: {username:<username>, password:<password>}
 */
 async function insertUser(data){
+    if(data == null || data.username == null || data.password == null){
+        throw new Error('You fucking idiot, you didnt give a username or password');
+    }
+
     data.user_id = Math.floor(Math.random()*100000);
     data.wins = 0;
     data.losses = 0;
@@ -28,6 +32,46 @@ async function insertUser(data){
     const userId = await userDocument.save();
     return userId;
 }
+
+/*
+userData: {username:<username>}
+*/
+async function getUsers(userData){
+    const users = await UserModel.find(userData);
+    console.log(users);
+    return users;
+}
+
+/*
+userData: {username:<username>}
+*/
+async function incrementUserWins(userData){
+    // Dont think too hard about this. I could figure out the update function so
+    // Instead ima just delete the old one and insert a new one with correct value ;)
+    const ud = await UserModel.findOneAndDelete(userData);
+    const newUserData = new UserModel({username:ud.username, password:ud.password, user_id:ud.user_id, wins:ud.wins+1, losses:ud.losses});
+    await newUserData.save();
+}
+
+/*
+userData: {username:<username>}
+*/
+async function incrementUserLosses(userData){
+    // Dont think too hard about this. I could figure out the update function so
+    // Instead ima just delete the old one and insert a new one with correct value ;)
+    const ud = await UserModel.findOneAndDelete(userData);
+    const newUserData = new UserModel({username:ud.username, password:ud.password, user_id:ud.user_id, wins:ud.wins, losses:ud.losses+1});
+    await newUserData.save();
+}
+
+async function eraseUsers(){
+    await UserModel.deleteMany();
+}
+
+// openDB().then(() => insertUser({username:'LuLumio14', password:'Password123'}).then(() => closeDB()));
+// openDB().then(() => incrementUserWins({username:'LuLumio13'}).then(() => closeDB()));
+// openDB().then(() => incrementUserLosses({username:'LuLumio13'}).then(() => closeDB()));
+openDB().then(() => getUsers().then(() => closeDB()));
 
 module.exports = {
     insertUser,
