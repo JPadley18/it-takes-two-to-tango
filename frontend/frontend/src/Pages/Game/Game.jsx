e319f5b900c657c97451b52adb58dd94d2c43861
Callum Shervill <shervc272@gmail.com>
2025-02-08 16:50:52 +0000

    fix: css changes

--- frontend/frontend/src/Pages/Game/Game.jsx
+++ frontend/frontend/src/Pages/Game/Game.jsx
@@ -4,6 +4,11 @@ import gamedata from "./game.json";
 import gamedata2 from "./game2.json";
 
 export default function Game() {
+  const printBothBoards = () => {
+    console.log(gamedata.data.board.symbols);
+    console.log(gamedata2.data.board.symbols);
+  };
+
   return (
     <div>
       <h1>Game</h1>
@@ -11,6 +16,9 @@ export default function Game() {
         <Board gamestate={gamedata} />
         <Board gamestate={gamedata2} />
       </div>
+      <button className="button-19" onClick={printBothBoards}>
+        Print Both Games
+      </button>
     </div>
   );
 }
