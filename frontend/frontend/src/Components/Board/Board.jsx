41ec36b52037fcce43aca3cf578db0c81953ba20
Joshua Martins-Borges <zlac269@live.rhul.ac.uk>
2025-02-08 22:29:41 +0000

    added contraints to display to grid

--- frontend/frontend/src/Components/Board/Board.jsx
+++ frontend/frontend/src/Components/Board/Board.jsx
@@ -5,6 +5,7 @@ import propTypes from "prop-types";
 export default function Board(props) {
   //console.log(props.gamestate);
   const [currentGame, setCurrentGame] = useState([]);
+  const modifierList = props.gamestate.data.board.modifiers;
   useEffect(() => {
     setCurrentGame(props.gamestate.data.board.symbols);
   }, [props.gamestate]);
@@ -27,6 +28,8 @@ export default function Board(props) {
 
   const printBoard = () => {
     console.log(currentGame);
+    console.log(modifierList);
+    // getModifier(modifierList,0,0)
   };
 
   return (
@@ -36,8 +39,10 @@ export default function Board(props) {
           {row.map((cell, j) => (
             <div key={j} className="cell" id={j} onClick={updateCell}>
               {cell}
+              <Modifier position={[modifierList,i,j]} same={true}/>
             </div>
           ))}
+          
         </div>
       ))}
       <button className="button-19" onClick={printBoard}>
@@ -49,3 +54,40 @@ export default function Board(props) {
 Board.propTypes = {
   gamestate: propTypes.json,
 };
+
+function getModifier(modifierList,i, j) {
+  // Iterate over each object in the array
+  for (let item of modifierList) {
+      // Check if the given i and j match x1-1 and y1-1
+      if (i === item.x1 - 1 && j === item.y1 - 1) {
+          if(i < item.x2-1){
+            return ['right', item.type];
+          }else if(i > item.x2-1){
+            return ['left', item.type];
+          }else if(j < item.y2-1){
+            return ['bottom', item.type];
+          }else{
+            return ['top', item.type];
+          }
+      }
+  }
+  return null;  // Return false if no matching pair is found
+}
+
+export function Modifier(props){
+  const side = getModifier(props.position[0], props.position[1], props.position[2]);
+  
+  console.log(side);
+  if(side == null){
+    return;
+  }else{
+    if(side[1] == 'same'){
+      // use =
+      return <a className={"modifier-symbol-"+side[0]}>{'='}</a>
+    }else{
+      return <a className={"modifier-symbol-"+side[0]} >{'✖'}</a>
+    }
+    
+  }
+  
+}
