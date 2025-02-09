7e1112252069aa9f0eff08b61e43f537b058b75f
Callum Shervill <shervc272@gmail.com>
2025-02-09 02:31:32 +0000

    frontend validations not working

--- frontend/frontend/src/Components/Board/Board.jsx
+++ frontend/frontend/src/Components/Board/Board.jsx
@@ -1,15 +1,57 @@
 import "./Board.css";
 import { useState, useEffect } from "react";
 import propTypes from "prop-types";
+//import Cassette from "../../Assets/cassette.png";
 
 export default function Board(props) {
   const [currentGame, setCurrentGame] = useState([]);
   const [modifierList, setModifierList] = useState([]);
 
+  // const validateBoard = () => {
+  //   // validate Linkdin Tango Rules
+  //   // 1. There cannot be 3 1's or 2's in a row or column
+  //   // If this is the case, highlight the row or column
+  //   // 2. must follow the modifier rules
+  //   // If does not follow rules, highlight the 2 cells that are not following the rules
+
+  //   // 1. Check for 3 in a row or column
+  //   for (let i = 0; i < 6; i++) {
+  //     // Check for columns
+  //     var column = [];
+  //     for (let j = 0; j < 6; j++) {
+  //       console.log(currentGame[j][i]);
+  //       column.push(currentGame[j][i]);
+  //     }
+  //     if (!rowIsValid(column)) {
+  //       console.log("Column " + i + " is invalid");
+  //     }
+  //     // Check for rows
+  //     if (!rowIsValid(currentGame[i])) {
+  //       console.log("Row " + i + " is invalid");
+  //     }
+  //   }
+  // };
+
+  // const rowIsValid = (row) => {
+  //   let count = 0;
+  //   for (let i = 0; i < row.length; i++) {
+  //     if (row[i] == 1 || row[i] == 2) {
+  //       count++;
+  //     } else {
+  //       count = 0;
+  //     }
+  //     if (count == 3) {
+  //       return false;
+  //     }
+  //   }
+  //   return true;
+  // };
+
   useEffect(() => {
     if (props.gamestate != undefined) {
       setCurrentGame(props.gamestate.spaces);
       setModifierList(props.gamestate.modifiers);
+      // validateBoard();
     } else {
       setCurrentGame([
         [0, 0, 0, 0, 0, 0],
@@ -58,6 +100,7 @@ export default function Board(props) {
           {row.map((cell, j) => (
             <div key={j} className="cell" id={j} onClick={updateCell}>
               {cell}
+              <img src="" alt="" />
               <Modifier position={[modifierList, j, i]} same={true} />
             </div>
           ))}
