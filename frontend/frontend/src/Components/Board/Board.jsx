7b6a496020a4eb64cca0dde201071e66930349a5
Jacob Padley <jacob5180@hotmail.co.uk>
2025-02-09 02:46:57 +0000

    hackowacko

--- frontend/frontend/src/Components/Board/Board.jsx
+++ frontend/frontend/src/Components/Board/Board.jsx
@@ -7,46 +7,6 @@ export default function Board(props) {
   const [currentGame, setCurrentGame] = useState([]);
   const [modifierList, setModifierList] = useState([]);
 
-  // const validateBoard = () => {
-  //   // validate Linkdin Tango Rules
-  //   // 1. There cannot be 3 1's or 2's in a row or column
-  //   // If this is the case, highlight the row or column
-  //   // 2. must follow the modifier rules
-  //   // If does not follow rules, highlight the 2 cells that are not following the rules
-
-  //   // 1. Check for 3 in a row or column
-  //   for (let i = 0; i < 6; i++) {
-  //     // Check for columns
-  //     var column = [];
-  //     for (let j = 0; j < 6; j++) {
-  //       console.log(currentGame[j][i]);
-  //       column.push(currentGame[j][i]);
-  //     }
-  //     if (!rowIsValid(column)) {
-  //       console.log("Column " + i + " is invalid");
-  //     }
-  //     // Check for rows
-  //     if (!rowIsValid(currentGame[i])) {
-  //       console.log("Row " + i + " is invalid");
-  //     }
-  //   }
-  // };
-
-  // const rowIsValid = (row) => {
-  //   let count = 0;
-  //   for (let i = 0; i < row.length; i++) {
-  //     if (row[i] == 1 || row[i] == 2) {
-  //       count++;
-  //     } else {
-  //       count = 0;
-  //     }
-  //     if (count == 3) {
-  //       return false;
-  //     }
-  //   }
-  //   return true;
-  // };
-
   useEffect(() => {
     if (props.gamestate != undefined) {
       setCurrentGame(props.gamestate.spaces);
@@ -76,7 +36,7 @@ export default function Board(props) {
 
     props.moveCallBack({
       command: "place_symbol",
-      placementPosition: { x: x, y: y, symbol: val },
+      placementPosition: { x, y, symbol: val },
     });
 
     x = parseInt(e.target.parentNode.id);
